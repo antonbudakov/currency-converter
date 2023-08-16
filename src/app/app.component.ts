bf8d14e8d54dde206835490a0fc52c360de01441
antonbudakov <buddanton@gmail.com>
2023-08-16 18:16:11 +0300

    Form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,72 +1,74 @@
 import { Component, OnInit } from '@angular/core';
 import { CurrenciesService } from './services/currencies.service';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
 onChangeClick() {
   let curr=this.form.value.currency1;
   this.form.controls['currency1'].patchValue(this.form.value.currency2);
   this.form.controls['currency2'].patchValue(curr);
 }
-  first: number=Number(localStorage.getItem('currency1_value'));
-  second: number=Number(localStorage.getItem('currency2_value'));
+  first: any;
+  second: any;
+  rate: number=0;
   title:string='Currency converter';
-  index_from=0;
-  index_to=1;
   currencies:any[]=[];
   form:FormGroup;
 
   constructor(private currenciesService:CurrenciesService,private formBuilder:FormBuilder){
-    currenciesService.get().subscribe(val=>this.currencies=val);
 
-    this.form=formBuilder.group({
-      currency1:null,
-      currency2:null,
-      sum1:0,
-      sum2:0
+    this.form=formBuilder.group({  
+      currency1:localStorage.getItem('currency1_value')||'EUR',  
+      currency2:localStorage.getItem('currency2_value')||'UAH',
+      sum1:[0,Validators.pattern('^[-+]?([0-9]+\.)?[0-9]+$')],
+      sum2:[0,Validators.pattern('^[-+]?([0-9]+\.)?[0-9]+$')]
     });
 
-    
+    currenciesService.get().subscribe(val=>{this.currencies=val;
+      this.currencies.unshift(
+        {"r030":36,"txt":"Українська гривня","rate":1,"cc":"UAH","exchangedate":"16.08.2023"});
+        this.first=this.currencies.find(x=>x.cc==this.form.value.currency1);  
+        this.second=this.currencies.find(x=>x.cc==this.form.value.currency2);
+        this.rate=this.first?.rate/this.second?.rate;
+    });
+
+    this.form.controls['currency1'].valueChanges.subscribe(val=>{
+      this.first=this.currencies.find(x=>x.cc==val);
+      localStorage.setItem('currency1_value',val);
+      this.calc();
+    });
+
+     this.form.controls['currency2'].valueChanges.subscribe(val=>{
+      this.second=this.currencies.find(x=>x.cc==val);
+      localStorage.setItem('currency2_value',val);
+      this.calc();     
+     });
   }
 
-  ngOnInit(): void {
-
-     this.form.controls['currency1'].valueChanges.subscribe(x=>{
-
-      this.form.controls['sum2'].patchValue(this.form.value?.sum1*x/this.form.value?.currency2);
-      localStorage.setItem('currency2_value',this.second?.toString());
-      localStorage.setItem('currency1_value',this.first?.toString());
-      
-    });
-
-     this.form.controls['currency2'].valueChanges.subscribe(x=>{
-      
-      this.form.controls['sum2'].patchValue(this.form.value?.sum1*this.form.value?.currency1/x);
-      localStorage.setItem('currency2_value',this.second?.toString());
-      localStorage.setItem('currency1_value',this.first?.toString());
-     });
+  calc(){
+    if (this.form.valid)
+    {
+      this.rate=this.first?.rate/this.second?.rate;
+      this.form.controls['sum2'].patchValue(Number(this.form.value?.sum1*this.rate));
+    }
   }
   
   firstInput(){  
-    this.first=this.form.value?.sum1;
-    //this.second=this.first*this.currencies[this.index_from].rate/this.currencies[this.index_to].rate;
-    this.form.controls['sum2'].patchValue(this.form.value?.sum1*
-      (this.form.value?.currency2?this.form.value?.currency1/this.form.value?.currency2:0));
-    localStorage.setItem('currency2_value',this.second?.toString());
-    localStorage.setItem('currency1_value',this.first?.toString());
+    if (this.form.valid)
+      this.form.controls['sum2'].patchValue(this.form.value?.sum1*(this.first.rate/this.second.rate));    
+    else
+      this.form.controls['sum2'].patchValue(0);
   }
 
   secondInput(){
-    // this.second=event.target.value;
-    // this.first=this.second*this.currencies[this.index_to].rate/this.currencies[this.index_from].rate;
-    this.form.controls['sum1'].patchValue(this.form.value?.sum2*
-      (this.form.value?.currency1?this.form.value?.currency2/this.form.value?.currency1:0));
-    localStorage.setItem('currency2_value',this.second?.toString());
-    localStorage.setItem('currency1_value',this.first?.toString());
+    if (this.form.valid)
+      this.form.controls['sum1'].patchValue(this.form.value?.sum2*(this.second.rate/this.first.rate))
+    else
+      this.form.controls['sum1'].patchValue(0);
   }
 }
